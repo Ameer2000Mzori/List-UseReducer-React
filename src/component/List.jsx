83bf6adee4ca48dfb29603984bf40940f3b45025
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-02-29 10:36:50 +0100

    added logic added reducer andded data and more

--- src/component/List.jsx
+++ src/component/List.jsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import React, { useState, useReducer, useEffect } from 'react'
+import { usersList } from './hooks/dataList.js'
+
+const reducer = (state, action) => {
+  if (action.type === 'CLEAR_LIST') {
+    return { state: {} }
+  }
+  if (action.type === 'REST_LIST') {
+    return { state: usersList }
+  }
+}
 
 const List = () => {
-  return <div>List</div>
+  const [userData, setUserData] = useState(usersList)
+
+  const [state, dispatch] = useReducer(reducer, userData)
+
+  const removeAllUsers = () => {
+    dispatch({ type: 'CLEAR_LIST' })
+  }
+
+  const resetListUsers = () => {
+    dispatch({ type: 'REST_LIST' })
+  }
+
+  useEffect(() => {
+    console.log(state)
+  }, [state])
+
+  return (
+    <div>
+      <div>
+        <button onClick={removeAllUsers}>Clear List</button>
+        <button onClick={resetListUsers}>Rest List</button>
+      </div>
+      <div>
+        {userData.map((user, index) => {
+          return (
+            <div key={index}>
+              <p>{user.name}</p>
+              <p>{user.age}</p>
+              <p>{user.id}</p>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default List
